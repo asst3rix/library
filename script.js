56f4b305654a7dc5ed953973131041b76ad75582
asst3rix_WC <185900998+asst3rix@users.noreply.github.com>
2025-11-25 14:27:50 +0100

    Create a myLibrary variable (const) that will store the books. Add an unique ID in the Book constructor. Create a function addBookToLibrary that create a book and add it to myLibrary. Add 3 books to myLibrary using addBookToLibrary.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+const myLibrary = [];
+
 // Book constructor
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.id = crypto.randomUUID();
 }
+
+function addBookToLibrary(title, author, pages, isRead) {
+    const newBook = new Book(title, author, pages, isRead);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Fellowship of the Ring", "J.R.R Tolkien", "423", "Yes");
+addBookToLibrary("The Two Towers", "J.R.R Tolkien", "352", "Yes");
+addBookToLibrary("The Return of the King", "J.R.R Tolkien", "416", "Yes");
