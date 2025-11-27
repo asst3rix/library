f3a42d0bf1cd0417c087674baea2ef869ed64da0
asst3rix_WC <185900998+asst3rix@users.noreply.github.com>
2025-11-27 10:59:28 +0100

    Create a booksList variable from #books-list. Manually add 4 books using addBookToLibrary(). Update the Book constructor to change the isRead parameter as boolean. Create the function displayBooks() that loops in the array of books and display them in #books-list. Create the function createTag() that create an HTML element giving a type, class and text.

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const myLibrary = [];
+const booksList = document.querySelector("#books-list");
 
-// Book constructor
+addBookToLibrary("The Fellowship of the Ring", "J.R.R Tolkien", "423", "Yes");
+addBookToLibrary("The Two Towers", "J.R.R Tolkien", "352", "Yes");
+addBookToLibrary("The Return of the King", "J.R.R Tolkien", "416", "Yes");
+addBookToLibrary("Vingt mille lieues sous les mers", "Jules Verne", "526", "No");
+
+displayBooks();
+
+// Book constructor.
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead;
+
+    if (isRead === "Yes") {
+        this.isRead = true;
+    } else {
+        this.isRead = false;
+    }
+
     this.id = crypto.randomUUID();
 }
 
@@ -14,6 +28,48 @@ function addBookToLibrary(title, author, pages, isRead) {
     myLibrary.push(newBook);
 }
 
-addBookToLibrary("The Fellowship of the Ring", "J.R.R Tolkien", "423", "Yes");
-addBookToLibrary("The Two Towers", "J.R.R Tolkien", "352", "Yes");
-addBookToLibrary("The Return of the King", "J.R.R Tolkien", "416", "Yes");
+function displayBooks() {
+    for (let index = 0; index < myLibrary.length; index++) {
+        const bookCard = createTag("div", "book-card", "");
+        booksList.appendChild(bookCard);
+
+        const bookTitle = createTag("h2", "", myLibrary[index].title)
+        bookCard.appendChild(bookTitle);
+
+        const bookAuthor = createTag("p", "", myLibrary[index].author)
+        bookCard.appendChild(bookAuthor);
+
+        const bookPages = createTag("p", "", myLibrary[index].pages + " pages")
+        bookCard.appendChild(bookPages);
+
+        const bookRead = createTag("div", "book-read", "");
+        bookCard.appendChild(bookRead);
+
+        const isRead = createTag("p", "", "Is read?")
+        bookRead.appendChild(isRead);
+
+        const readButton = createTag("button", "is-read-button", "");
+
+        if (myLibrary[index].isRead) {
+            readButton.classList.add("read");
+            readButton.textContent = "Yes";
+        } else {
+            readButton.classList.add("not-read");
+            readButton.textContent = "No";
+        }
+
+        bookRead.appendChild(readButton);
+    }
+}
+
+// Function that create an HTML element giving a type, class and text.
+function createTag(type, classs, text) {
+    const tag = document.createElement(type);
+    if (classs != "") {
+        tag.classList.add(classs);
+    }
+    if (text != "") {
+        tag.textContent = text;
+    }
+    return tag;
+}
