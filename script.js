1f19929e25c4e5e9400e235e0b27131e8299b8d6
asst3rix_WC <185900998+asst3rix@users.noreply.github.com>
2025-12-03 15:59:45 +0100

    Add event listener on the validationButton. > Retrieve the values of the inputs and create a book with it. > Reset the form, close the dialog and display the books. Replace "Yes" by 1 in the book constructor following changing the value property of the radio buttons. Add a line at the beginning of displayBooks() that delete everything that could be here before starting showing the books.

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const booksList = document.querySelector("#books-list");
 const dialog = document.querySelector("dialog");
 const addButton = document.querySelector("#add-button");
 const closeButton = document.querySelector("#close");
+const validationButton = document.querySelector("#validation-button");
+const titleInput = document.querySelector("#book-title");
+const authorInput = document.querySelector("#book-author");
+const pagesInput = document.querySelector("#book-pages");
+
+addBookToLibrary("The Fellowship of the Ring", "J.R.R Tolkien", "423", "1");
+addBookToLibrary("The Two Towers", "J.R.R Tolkien", "352", "1");
+addBookToLibrary("The Return of the King", "J.R.R Tolkien", "416", "1");
+addBookToLibrary("Vingt mille lieues sous les mers", "Jules Verne", "526", "0");
+
+displayBooks();
 
 addButton.addEventListener("click", () => {
     dialog.showModal();
@@ -12,12 +23,24 @@ closeButton.addEventListener("click", () => {
     dialog.close();
 });
 
-addBookToLibrary("The Fellowship of the Ring", "J.R.R Tolkien", "423", "Yes");
-addBookToLibrary("The Two Towers", "J.R.R Tolkien", "352", "Yes");
-addBookToLibrary("The Return of the King", "J.R.R Tolkien", "416", "Yes");
-addBookToLibrary("Vingt mille lieues sous les mers", "Jules Verne", "526", "No");
+validationButton.addEventListener("click", (event) => {
+    event.preventDefault();
 
-displayBooks();
+    // We do it now because when loading the page, there is no radio button checked so this return 'null'.
+    const selector = 'input[name="isread"]:checked';
+    const isReadInput = document.querySelector(selector);
+
+    addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, isReadInput.value);
+
+    // Reset the form.
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    isReadInput.checked = false;
+
+    dialog.close();
+    displayBooks();
+});
 
 // Book constructor.
 function Book(title, author, pages, isRead) {
@@ -25,7 +48,7 @@ function Book(title, author, pages, isRead) {
     this.author = author;
     this.pages = pages;
 
-    if (isRead === "Yes") {
+    if (isRead === "1") {
         this.isRead = true;
     } else {
         this.isRead = false;
@@ -40,6 +63,9 @@ function addBookToLibrary(title, author, pages, isRead) {
 }
 
 function displayBooks() {
+    // Delete everything that could be here before starting showling the books.
+    booksList.innerHTML = '';
+
     for (let index = 0; index < myLibrary.length; index++) {
         const bookCard = createTag("div", "book-card", "");
         booksList.appendChild(bookCard);
