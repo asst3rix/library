96a89bd41484b1785ec871d7091ef91b234f8e8d
asst3rix_WC <185900998+asst3rix@users.noreply.github.com>
2025-12-04 15:21:16 +0100

    Create the function createSVG() that create the SVG icon to delete a book. Modify displayBooks() so it adds the delete button.

--- script.js
+++ script.js
@@ -70,19 +70,20 @@ function displayBooks() {
         const bookCard = createTag("div", "book-card", "");
         booksList.appendChild(bookCard);
 
-        const bookTitle = createTag("h2", "", myLibrary[index].title)
-        bookCard.appendChild(bookTitle);
+        const bookDiv = createTag("div", "", "");
+        const bookTitle = createTag("h2", "", myLibrary[index].title);
+        bookDiv.appendChild(bookTitle);
 
-        const bookAuthor = createTag("p", "", myLibrary[index].author)
-        bookCard.appendChild(bookAuthor);
+        const bookAuthor = createTag("p", "", myLibrary[index].author);
+        bookDiv.appendChild(bookAuthor);
 
-        const bookPages = createTag("p", "", myLibrary[index].pages + " pages")
-        bookCard.appendChild(bookPages);
+        const bookPages = createTag("p", "", myLibrary[index].pages + " pages");
+        bookDiv.appendChild(bookPages);
 
         const bookRead = createTag("div", "book-read", "");
-        bookCard.appendChild(bookRead);
+        bookDiv.appendChild(bookRead);
 
-        const isRead = createTag("p", "", "Is read?")
+        const isRead = createTag("p", "", "Is read?");
         bookRead.appendChild(isRead);
 
         const readButton = createTag("button", "is-read-button", "");
@@ -96,6 +97,9 @@ function displayBooks() {
         }
 
         bookRead.appendChild(readButton);
+
+        bookCard.appendChild(bookDiv);
+        bookCard.appendChild(createSVG());
     }
 }
 
@@ -109,4 +113,19 @@ function createTag(type, classs, text) {
         tag.textContent = text;
     }
     return tag;
+}
+
+function createSVG() {
+    const svgNS = "http://www.w3.org/2000/svg";
+    const svgDelete = document.createElementNS(svgNS, "svg");
+    svgDelete.classList.add("delete-book");
+    svgDelete.setAttribute("xmlns", svgNS);
+    svgDelete.setAttribute("viewBox", "0 0 24 24");
+    const titleElement = document.createElementNS(svgNS, "title");
+    titleElement.textContent = "delete";
+    svgDelete.appendChild(titleElement);
+    const pathElement = document.createElementNS(svgNS, "path");
+    pathElement.setAttribute("d", "M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z");
+    svgDelete.appendChild(pathElement);
+    return svgDelete;
 }
