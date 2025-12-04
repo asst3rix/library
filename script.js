935d2b34d31718e7eef5280779e2b6688a517822
asst3rix_WC <185900998+asst3rix@users.noreply.github.com>
2025-12-04 17:45:05 +0100

    Add event listener on the isread buttons that change the isread status of the associated book. Create a function updateReadStatus().

--- script.js
+++ script.js
@@ -87,6 +87,7 @@ function displayBooks() {
         bookRead.appendChild(isRead);
 
         const readButton = createTag("button", "is-read-button", "");
+        readButton.dataset.id = myLibrary[index].id;
 
         if (myLibrary[index].isRead) {
             readButton.classList.add("read");
@@ -95,6 +96,21 @@ function displayBooks() {
             readButton.classList.add("not-read");
             readButton.textContent = "No";
         }
+
+        // Use of a function instead of an arrow function to be able to use "this".
+        readButton.addEventListener("click", function () {
+            if (this.textContent === "Yes") {
+                this.textContent = "No";
+                this.classList.remove("read");
+                this.classList.add("not-read");
+                updateReadStatus(this.dataset.id, false);
+            } else {
+                this.textContent = "Yes";
+                this.classList.remove("not-read");
+                this.classList.add("read");
+                updateReadStatus(this.dataset.id, true);
+            }
+        });
 
         bookRead.appendChild(readButton);
         bookCard.appendChild(bookDiv);
@@ -141,4 +157,13 @@ function createSVG(id) {
     pathElement.setAttribute("d", "M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z");
     svgDelete.appendChild(pathElement);
     return svgDelete;
+}
+
+function updateReadStatus(id, status) {
+    for (let index = 0; index < myLibrary.length; index++) {
+        if (myLibrary[index].id === id) {
+            myLibrary[index].isRead = status;
+            break;
+        }
+    }
 }
